6ce64940445437b05c2799672f0b6bc43b250014
Tomek Wytrebowicz <tomalecpub@gmail.com>
2014-08-14 01:50:26 +0200

    Let the server decide about uploaded file's url

--- test/mockServer.js
+++ test/mockServer.js
@@ -5,13 +5,15 @@
 		console.info("Mock server caught `/storage/server/path` request,"+
 			" and now is adding timestamp to file name")
 		var xFile = JSON.parse(xhr.requestHeaders['x-file']);
+		var savedName = Date.now() + xFile.name;
 		xhr.respond(
 			200, 
 			{ 
 				"Content - Type" : "application / json",
 				"x-file" : JSON.stringify( { 
 					type: xFile.type, 
-					name: Date.now() + xFile.name 
+					name: savedName,
+					url: "/storage/server/files/" + savedName
 				} )
 			}, 
 			JSON.stringify([{ id: 1, text: "Provide examples", done: true }])
