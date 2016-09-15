ecdb49f5ba0f3df60e61482970c6da30c373ebf8
Marcin Warpechowski <warpech@gmail.com>
2016-09-15 11:37:58 +0200

    read previous version number from bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
-var pjson = require('./package.json');
+var packageJson = require('./package.json');
+var bowerJson = require('./bower.json');
 
 gulp.task('propagate-version', function() {
-  gulp.src(['./bower.json'])
-    .pipe(replace(/"version": "\d+\.\d+\.\d+"/, '"version": "' + pjson.version + '"'))
-    .pipe(gulp.dest('./'));
-
-  gulp.src(['./juicy-filedrop.html'])
-    .pipe(replace(/version: \d+\.\d+\.\d+/, 'version: ' + pjson.version))
+  gulp.src(['./bower.json', './juicy-filedrop.html'])
+    .pipe(replace(bowerJson.version, packageJson.version))
     .pipe(gulp.dest('./'));
 });
