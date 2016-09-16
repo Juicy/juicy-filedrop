41ae616301e7e286920115c31153537093d5c4b9
Marcin Warpechowski <warpech@gmail.com>
2016-09-16 10:07:26 +0200

    simplicity: just use a regexp to replace the old version number

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,10 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
-var spawn = require('child_process').spawnSync;
 
 gulp.task('propagate-version', function() {
   var next = require('./package.json');
 
-  //current working directory already have a new version number after `npm version`
-  //old version number is available in git history, let's take it
-  var git = spawn('git', ['show', 'HEAD:./package.json']);
-  var out = git.stdout.toString()
-  var old = JSON.parse(out);
-
   gulp.src(['./juicy-filedrop.html'])
-    .pipe(replace(old.version, next.version))
+    .pipe(replace(/version: (.*)/, 'version: ' + next.version))
     .pipe(gulp.dest('./'));
 });
