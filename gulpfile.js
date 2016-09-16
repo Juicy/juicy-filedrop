a41df22c6e1f0c5ab704055ebabc54d24d5f6759
Marcin Warpechowski <warpech@gmail.com>
2016-09-16 10:03:29 +0200

    remove version from bower.json (redundant), use git history to retrieve old version number

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
-var packageJson = require('./package.json');
-var bowerJson = require('./bower.json');
+var spawn = require('child_process').spawnSync;
 
 gulp.task('propagate-version', function() {
-  gulp.src(['./bower.json', './juicy-filedrop.html'])
-    .pipe(replace(bowerJson.version, packageJson.version))
+  var next = require('./package.json');
+
+  //current working directory already have a new version number after `npm version`
+  //old version number is available in git history, let's take it
+  var git = spawn('git', ['show', 'HEAD:./package.json']);
+  var out = git.stdout.toString()
+  var old = JSON.parse(out);
+
+  gulp.src(['./juicy-filedrop.html'])
+    .pipe(replace(old.version, next.version))
     .pipe(gulp.dest('./'));
 });
